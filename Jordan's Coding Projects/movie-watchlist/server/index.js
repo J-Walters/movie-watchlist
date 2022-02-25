3d11d3c09bbe188b90e5aa2c165a98e453c22ca0
Jordan Walters <jwalters012@gmail.com>
2022-02-25 17:24:09 -0500

    add movie form

--- Jordan's Coding Projects/movie-watchlist/server/index.js
+++ Jordan's Coding Projects/movie-watchlist/server/index.js
@@ -13,12 +13,19 @@ const startServer = async () => {
 }
 startServer()
 
+//matches any url for a GET request to a possible file
+//in the public directory
+app.use(express.static(__dirname + "/public"))
+
 //start of all middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 const genresRouter = require('./routes/genre')
 app.use("/genre", genresRouter);
+
+const moviesRouter = require('./routes/movie')
+app.use('/movies', moviesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello :)');
